86778b0ac983fd583c0445f5ba6b27fc04cfea72
QuasarIIVII <zest8011@gmail.com>
2024-07-24 02:52:31 +0000

    init

---
+++ net.acmicpc/a.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<memory>
+#include<array>
+#include<tuple>
+#include<bitset>
+#include<map>
+#include<<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<functional>
+#include<algorithm>
+#include<cmath>
+
+using namespace std;
+
+using u1=uint8_t;	using u2=uint16_t;	using u4=uint32_t;	using u8=uint64_t;	using u16=unsigned __int128;
+using i1=int8_t;	using i2=int16_t;	using i4=int32_t;	using i8=int64_t;	using i16=__int128;
+										using f4=float;		using f8=double;	using f16=long double;
+using uf1=uint_fast8_t;	using uf2=uint_fast16_t;using uf4=uint_fast32_t;using uf8=uint_fast64_t;
+using if1=int_fast8_t;	using if2=int_fast16_t;	using if4=int_fast32_t;	using if8=int_fast64_t;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	return 0;
+}
+
---
+++ net.acmicpc/createAsm64.sh
@@ -0,0 +1,18 @@
+# $1 : folder name
+#!/bin/bash
+
+if [ -z $1 ]; then
+	echo "Usage: $0 <foldername>"
+	exit 1
+fi
+
+if [ -d $1 ]; then
+	echo "Folder $1 already exists"
+	exit 2
+fi
+
+mkdir $1 && cd $1
+mkdir build
+cp ../a.asm .
+cp ../Makefile build
+nvim a.asm
