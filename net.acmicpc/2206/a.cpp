1622ff71e1d38fcb10a6d65c90027dc24e7d7a3b
QuasarIIVII <quasariivii@gmail.com>
2024-11-16 01:13:34 +0900

    2206 sub.no. 86489251

--- net.acmicpc/2206/a.cpp
+++ net.acmicpc/2206/a.cpp
@@ -18,6 +18,9 @@
 #include<cmath>
 #include<cstring>
 
+#include<chrono>
+#include<thread>
+
 using namespace std;
 
 using u1=uint8_t;	using u2=uint16_t;	using u4=uint32_t;	using u8=uint64_t;	using u16=unsigned __int128;
@@ -34,6 +37,150 @@ constexpr bool debug=true;
 
 #define DEBUG if constexpr(debug)
 #define DEBUG_BLOCK(x) if constexpr(debug){x}
+
+template<class T>
+struct Alloc_d: public allocator<T>{
+	using Base = std::allocator<T>;
+    using value_type = typename Base::value_type;
+
+    Alloc_d() = default;
+    
+    template <typename U>
+    Alloc_d(const Alloc_d<U>& other) noexcept : Base(other) {}
+
+    T* allocate(std::size_t n) {
+        T* ptr = Base::allocate(n);
+        DEBUG std::cout
+			<< "Allocating " << n
+			<< " element(s) of size " << sizeof(T)
+			<< " at address " << static_cast<void*>(ptr) << '\n';
+        return ptr;
+    }
+
+    void deallocate(T* ptr, std::size_t n) noexcept {
+        DEBUG std::cout
+			<< "Deallocating " << n
+			<< " element(s) of size " << sizeof(T)
+			<< " from address " << static_cast<void*>(ptr) << '\n';
+        Base::deallocate(ptr, n);
+    }
+};
+
+template<class T,size_t N=4096>
+struct Alloc_static{
+	using value_type		= T;
+	using size_type			= size_t;
+	using difference_type	= ptrdiff_t;
+	using propagate_on_container_move_assignment = true_type;
+
+	array<T,N> data;
+	array<size_t,N> map={0,};
+	bitset<N> isAvailable;
+
+	constexpr Alloc_static() noexcept {
+		DEBUG cout<<"Alloc_static<T,"<<N<<'>'<<endl;
+		map[0]=N;
+		isAvailable[0]=true;
+		DEBUG cout<<isAvailable[0]<<endl;
+	}
+	constexpr Alloc_static(const Alloc_static& other) noexcept : data(other.data) {
+		DEBUG cout<<"Alloc_static<T,"<<N<<">(const Alloc_static&)"<<endl;
+		map[0]=N;
+		isAvailable[0]=true;
+	}
+	template<class U,size_t M>
+	constexpr Alloc_static(const Alloc_static<U,M>& other) noexcept {
+		DEBUG cout<<"Alloc_static<T,"<<N<<">(const Alloc_static<"<<N<<','<<M<<">&)"<<endl;
+		map[0]=N;
+		isAvailable[0]=true;
+	}
+
+	template<class U>
+	struct rebind{
+		using other=Alloc_static<U,N>;
+	};
+
+	T* allocate(std::size_t n){
+		DEBUG cout<<"Allocating "<<n<<" elements"<<endl;
+		size_t i, j;
+		DEBUG cout<<isAvailable[0]<<endl;
+		for(i=0; i<N && !isAvailable[i]; i+=map[i])DEBUG cout<<i<<' ';
+		while(i<N && map[i]<n){
+			for(j=i+map[i]; j<N && isAvailable[j] && map[i]<n;){
+				map[i]+=map[j];
+				size_t jj=j+map[j];
+				map[j]=0;
+				j=jj;
+			}
+			for(i=j; i<N && !isAvailable[i]; i+=map[i]);
+		}
+
+		// failed
+		if(N<=i || map[i]<n){
+			DEBUG cout<<"Failed to allocate "<<n<<" elements"<<endl;
+			return nullptr;
+		}
+
+		if(map[i]==n){ // success
+			isAvailable[i]=false;
+			DEBUG cout<<"Allocated "<<n<<" elements at "<<i<<endl;
+			return data.data()+i;
+		}
+
+		// success (split space)
+		size_t sz=map[i];
+		map[i]=n;
+		isAvailable[i]=false;
+		map[i+n]=sz-n;
+		isAvailable[i+n]=true;
+
+		DEBUG cout<<"Allocated "<<n<<" elements at "<<i<<endl;
+		return data.data()+i;
+	}
+
+	void deallocate(T* p, std::size_t n){
+		ptrdiff_t i=p-data.data();
+		isAvailable[i]=true;
+	}
+};
+
+template <typename T, std::size_t Alignment = alignof(T)>
+class MyAllocator {
+public:
+    using value_type = T;
+
+    MyAllocator() noexcept = default;
+
+    template <typename U, std::size_t OtherAlignment>
+    MyAllocator(const MyAllocator<U, OtherAlignment>&) noexcept {}
+
+    // Rebind allocator to another type
+    template <typename U>
+    struct rebind {
+        using other = MyAllocator<U, Alignment>;
+    };
+
+    T* allocate(std::size_t n) {
+        std::cout << "Allocating " << n << " elements aligned to " << Alignment << " bytes\n";
+        
+        // Ensure the total allocation size is a multiple of the alignment
+        std::size_t size = n * sizeof(T);
+        if (size % Alignment != 0) {
+            size += Alignment - (size % Alignment);
+        }
+
+        if (auto ptr = static_cast<T*>(std::aligned_alloc(Alignment, size))) {
+            return ptr;
+        } else {
+            throw std::bad_alloc();
+        }
+    }
+
+    void deallocate(T* p, std::size_t) noexcept {
+        std::cout << "Deallocating\n";
+        std::free(p); // Use free to deallocate memory from aligned_alloc
+    }
+};
 
 array<char[1000],1000> a;
 array<array<array<uf4,2>,1000>,1000> d;
@@ -70,7 +217,7 @@ int main(){
 		cout<<"\033[49m";
 	)
 
-	queue<S1> q;
+	queue<S1,deque<S1,Alloc_static<S1,16384>>> q;
 	q.emplace(sx,sy,0);
 	d[q.front().x][q.front().y]={1,1};
 
@@ -109,31 +256,3 @@ int main(){
 
 	return 0;
 }
-
-template<class T>
-struct Alloc_d: public allocator<T>{
-	using Base = std::allocator<T>;
-    using value_type = typename Base::value_type;
-
-    Alloc_d() = default;
-    
-    template <typename U>
-    Alloc_d(const Alloc_d<U>& other) noexcept : Base(other) {}
-
-    T* allocate(std::size_t n) {
-        T* ptr = Base::allocate(n);
-        DEBUG std::cout
-			<< "Allocating " << n
-			<< " element(s) of size " << sizeof(T)
-			<< " at address " << static_cast<void*>(ptr) << '\n';
-        return ptr;
-    }
-
-    void deallocate(T* ptr, std::size_t n) noexcept {
-        DEBUG std::cout
-			<< "Deallocating " << n
-			<< " element(s) of size " << sizeof(T)
-			<< " from address " << static_cast<void*>(ptr) << '\n';
-        Base::deallocate(ptr, n);
-    }
-};
