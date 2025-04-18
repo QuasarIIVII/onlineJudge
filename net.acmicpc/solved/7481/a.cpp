e868b8c7e0fea5a669d3d0617951316f3927bd80
QuasarIIVII <quasariivii@gmail.com>
2025-04-18 10:12:57 +0900

    @ 2025-04-18T01:12:57Z

---
+++ net.acmicpc/solved/7481/a.cpp
@@ -0,0 +1,91 @@
+//; echo """
+#include<iostream>
+#include<sstream>
+#include<string>
+#include<string_view>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<memory>
+#include<array>
+#include<tuple>
+#include<bitset>
+#include<map>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<functional>
+#include<algorithm>
+#include<cmath>
+#include<cstring>
+
+using namespace std;
+
+using u1=uint8_t;	using u2=uint16_t;	using u4=uint32_t;	using u8=uint64_t;	using u16=unsigned __int128;
+using i1=int8_t;	using i2=int16_t;	using i4=int32_t;	using i8=int64_t;	using i16=__int128;
+										using f4=float;		using f8=double;	using f16=long double;
+using uf1=uint_fast8_t;	using uf2=uint_fast16_t;using uf4=uint_fast32_t;using uf8=uint_fast64_t;
+using if1=int_fast8_t;	using if2=int_fast16_t;	using if4=int_fast32_t;	using if8=int_fast64_t;
+
+#ifdef ONLINE_JUDGE
+constexpr bool debug=false;
+#else
+constexpr bool debug=true;
+#endif
+
+#ifdef ONLINE_JUDGE
+#define DEBUG_MACRO(x)
+#define DEBUG_MACRO_ELSE(x) x
+#else
+#define DEBUG_MACRO(x) x
+#define DEBUG_MACRO_ELSE(x)
+#endif
+
+#define DEBUG if constexpr(debug)
+#define DEBUG_BLOCK(x) if constexpr(debug){x}
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	uf4 T;
+	cin>>T;
+	while(T--){
+		uf4 a, b, c;
+		cin>>a>>b>>c;
+
+		bool f=false;
+		if(b<a)
+			f=true, swap(a, b);
+
+		uf4 x=c/b, i;
+		c = c%b;
+		bitset<10000> bb;
+
+		for(i=x+1; i--;){
+			uf4 r = c%a;
+
+			if(bb.test(r)){
+				reinterpret_cast<if4&>(i)=-1;
+				break;
+			}
+
+			bb.set(r);
+			if(!r){
+				f
+					?cout<<i<<' '<<c/a<<'\n'
+					:cout<<c/a<<' '<<i<<'\n';
+				break;
+			}
+			c+=b;
+		}
+		if(static_cast<if4>(i)<0){
+			cout<<"Impossible\n";
+			continue;
+		}
+	}
+	return 0;
+}
+
