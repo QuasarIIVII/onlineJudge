f0ffab070917406dca9ddff5a74fa27f872439ec
QuasarIIVII <quasariivii@gmail.com>
2025-06-05 17:22:03 +0900

    @ 2025-06-05T08:22:03Z

---
+++ net.acmicpc/solved/9019/a.cpp
@@ -0,0 +1,99 @@
+//; echo """
+#include<iostream>
+#include<sstream>
+#include<string>
+#include<string_view>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<memory>
+#include<array>
+#include<tuple>
+#include<bitset>
+#include<map>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<functional>
+#include<algorithm>
+#include<cmath>
+#include<cstring>
+
+using namespace std;
+
+using u1=uint8_t;	using u2=uint16_t;	using u4=uint32_t;	using u8=uint64_t;	using u16=unsigned __int128;
+using i1=int8_t;	using i2=int16_t;	using i4=int32_t;	using i8=int64_t;	using i16=__int128;
+										using f4=float;		using f8=double;	using f16=long double;
+using uf1=uint_fast8_t;	using uf2=uint_fast16_t;using uf4=uint_fast32_t;using uf8=uint_fast64_t;
+using if1=int_fast8_t;	using if2=int_fast16_t;	using if4=int_fast32_t;	using if8=int_fast64_t;
+
+#ifdef ONLINE_JUDGE
+constexpr bool debug=false;
+#else
+constexpr bool debug=true;
+#endif
+
+#ifdef ONLINE_JUDGE
+#define DEBUG_MACRO(x)
+#define DEBUG_MACRO_ELSE(x) x
+#else
+#define DEBUG_MACRO(x) x
+#define DEBUG_MACRO_ELSE(x)
+#endif
+
+#define DEBUG if constexpr(debug)
+#define DEBUG_BLOCK(x) if constexpr(debug){x}
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	uf4 T;
+	cin>>T;
+
+	while(T--){
+		uf2 x, y;
+		cin>>x>>y;
+
+		array<uf2, 10000> a;
+		array<pair<uf2, char>, 10000> b;
+		memset(a.data(), 0xff, sizeof(uf2) * a.size());
+
+		queue<uf2> q;
+
+		a[x] = 0;
+		q.push(x);
+		while(q.size()){
+			uf2 n = q.front();
+			q.pop();
+
+			const array<pair<char, uf2>, 4> m = {
+				make_pair('D', n*2 % 10000),
+				make_pair('S', n ? n-1 : 9999),
+				make_pair('R', n/10 + (n%10)*1000),
+				make_pair('L', (n%1000)*10 + n/1000)
+			};
+			for(auto& mm : m){
+				if(uf2 t = a[n] + 1; t < a[mm.second]){
+					a[mm.second] = t;
+					b[mm.second] = make_pair(n, mm.first);
+					q.push(mm.second);
+				}
+			}
+		}
+
+		stack<char> s;
+		for(uf2 p = y; p != x; p = b[p].first)
+			s.push(b[p].second);
+
+		while(s.size()){
+			cout<<s.top();
+			s.pop();
+		}
+		cout<<'\n';
+	}
+	return 0;
+}
+//; echo """
