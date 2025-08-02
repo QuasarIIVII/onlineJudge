bedfb7c7b7c93929628ab89736616bbc4398397d
QuasarIIVII <quasariivii@gmail.com>
2025-08-02 22:08:12 +0900

    @ 2025-08-02T13:08:12Z

---
+++ net.acmicpc/solved/16953/a.cpp
@@ -0,0 +1,81 @@
+//; echo """
+#include<iostream>
+#include<sstream>
+#include<string>
+#include<string_view>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<memory>
+#include<array>
+#include<tuple>
+#include<bitset>
+#include<map>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<functional>
+#include<algorithm>
+#include<cmath>
+#include<cstring>
+//; echo """
+
+using namespace std;
+
+using u1=uint8_t;	using u2=uint16_t;	using u4=uint32_t;	using u8=uint64_t;	using u16=unsigned __int128;
+using i1=int8_t;	using i2=int16_t;	using i4=int32_t;	using i8=int64_t;	using i16=__int128;
+										using f4=float;		using f8=double;	using f16=long double;
+using uf1=uint_fast8_t;	using uf2=uint_fast16_t;using uf4=uint_fast32_t;using uf8=uint_fast64_t;
+using if1=int_fast8_t;	using if2=int_fast16_t;	using if4=int_fast32_t;	using if8=int_fast64_t;
+
+#ifdef ONLINE_JUDGE
+constexpr bool debug=false;
+#else
+constexpr bool debug=true;
+#endif
+
+#ifdef ONLINE_JUDGE
+#define DEBUG_MACRO(x)
+#define DEBUG_MACRO_ELSE(x) x
+#else
+#define DEBUG_MACRO(x) x
+#define DEBUG_MACRO_ELSE(x)
+#endif
+
+#define DEBUG if constexpr(debug)
+
+bitset<1'000'000'001> v;
+
+int main(){
+	cin.tie(0)->sync_with_stdio(false);
+
+	uf4 n, m;
+	cin>>n>>m;
+
+	queue<pair<uf4, uf1>> q;
+	q.emplace(m, 1);
+	v.set(m);
+
+	for(; q.size(); q.pop()){
+		const auto &[cur, cnt] = q.front();
+		if(cur == n){
+			cout<<static_cast<uf2>(cnt);
+			return 0;
+		}
+		if(cur % 10 == 1){
+			const uf4 nxt = cur / 10;
+			if(nxt && !v.test(nxt))
+				q.emplace(nxt, cnt+1);
+		}
+		if(!(cur&1)){
+			const uf4 nxt = cur/2;
+			if(nxt && !v.test(nxt))
+				q.emplace(nxt, cnt+1);
+		}
+	}
+	cout<<"-1";
+	return 0;
+}
+//; echo """
