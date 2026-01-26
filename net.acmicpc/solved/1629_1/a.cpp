a36419a5d6d7b8e609306ed270642b4612278cd6
QuasarIIVII <zest8011@gmail.com>
2026-01-26 08:52:19 +0000

    @ 2026-01-26T08:52:19Z

---
+++ net.acmicpc/solved/1629_1/a.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<sstream>
+#include<string>
+#include<string_view>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<memory>
+#include<array>
+#include<tuple>
+#include<bitset>
+#include<map>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<functional>
+#include<algorithm>
+#include<stdfloat>
+#include<cmath>
+#include<cstring>
+#define AFESDJPOI asm("")
+//$(pwd); cat <<AFESDJPOI
+
+using namespace std;
+
+using u1=uint8_t;	using u2=uint16_t;	using u4=uint32_t;	using u8=uint64_t;	using u16=unsigned __int128;
+using i1=int8_t;	using i2=int16_t;	using i4=int32_t;	using i8=int64_t;	using i16=__int128;
+										using f4=float32_t;	using f8=float64_t;	using f16=float128_t;
+using uf1=uint_fast8_t;	using uf2=uint_fast16_t;using uf4=uint_fast32_t;using uf8=uint_fast64_t;
+using if1=int_fast8_t;	using if2=int_fast16_t;	using if4=int_fast32_t;	using if8=int_fast64_t;
+
+#ifdef ONLINE_JUDGE
+constexpr bool debug=false;
+#else
+constexpr bool debug=true;
+#endif
+
+#ifdef ONLINE_JUDGE
+#define DEBUG_MACRO(x)
+#define DEBUG_MACRO_ELSE(x) x
+#else
+#define DEBUG_MACRO(x) x
+#define DEBUG_MACRO_ELSE(x)
+#endif
+
+#define DEBUG if constexpr(debug)
+
+int main(){
+	cin.tie(0)->sync_with_stdio(false);
+
+	u8 a, b, c;
+	cin>>a>>b>>c;
+
+	a%=c;
+	function<u8(u8)> f = [&](u8 e) -> u8 {
+		if(e==0) return 1;
+		if(e==1) return a;
+		u16 r = f(e/2);
+		return static_cast<u8>(e&1 ? r*r*a : r*r)%c;
+	};
+
+	cout<<f(b);
+	return 0;
+}
+AFESDJPOI
+;
