11c290a06f7f05cc818c8a64c1f8094f841bbc4d
QuasarIIVII <zest8011@gmail.com>
2026-03-16 07:26:46 +0000

    @ 2026-03-16T07:26:46Z

---
+++ net.acmicpc/solved/1002/a.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+#include<sstream>
+#include<string>
+#include<string_view>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<memory>
+#include<array>
+#include<tuple>
+#include<bitset>
+#include<map>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<functional>
+#include<algorithm>
+#include<stdfloat>
+#include<cmath>
+#include<cstring>
+#define AFESDJPOI asm("")
+//$(pwd); cat <<AFESDJPOI
+
+using namespace std;
+
+using u1=uint8_t;	using u2=uint16_t;	using u4=uint32_t;	using u8=uint64_t;	using u16=unsigned __int128;
+using i1=int8_t;	using i2=int16_t;	using i4=int32_t;	using i8=int64_t;	using i16=__int128;
+										using f4=float32_t;	using f8=float64_t;	using f16=float128_t;
+using uf1=uint_fast8_t;	using uf2=uint_fast16_t;using uf4=uint_fast32_t;using uf8=uint_fast64_t;
+using if1=int_fast8_t;	using if2=int_fast16_t;	using if4=int_fast32_t;	using if8=int_fast64_t;
+
+#ifdef ONLINE_JUDGE
+constexpr bool debug=false;
+#else
+constexpr bool debug=true;
+#endif
+
+#ifdef ONLINE_JUDGE
+#define DEBUG_MACRO(x)
+#define DEBUG_MACRO_ELSE(x) x
+#else
+#define DEBUG_MACRO(x) x
+#define DEBUG_MACRO_ELSE(x)
+#endif
+
+#define DEBUG if constexpr(debug)
+
+int main(){
+	cin.tie(0)->sync_with_stdio(false);
+
+	u4 T;
+	cin>>T;
+	while(T--){
+		i4 x0,y0,r0,x1,y1,r1;
+		cin>>x0>>y0>>r0>>x1>>y1>>r1;
+
+		const i4 dx = x0-x1, dy = y0-y1;
+		const i4 d2 = dx*dx + dy*dy;
+		const i4 r = r0+r1;
+		const i4 r2 = r*r;
+		const auto [rmx, rmn]
+			= r0 < r1
+			? pair{r1, r0} : pair{r0, r1};
+		const i4 rr = rmx-rmn;
+		const i4 rr2 = rr*rr;
+
+		if(x0==x1 && y0==y1)
+			cout<<(r0==r1 ? "-1\n" : "0\n");
+		else if(d2 < rr2) cout<<"0\n";
+		else if(d2==rr2) cout<<"1\n";
+		else if(d2==r2) cout<<"1\n";
+		else if(d2>r2) cout<<"0\n";
+		else cout<<"2\n";
+	}
+	return 0;
+}
+AFESDJPOI
+;
