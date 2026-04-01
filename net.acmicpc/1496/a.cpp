cb783bb05029cca56b49ff691a9bb4bc9e5b6567
QuasarIIVII <zest8011@gmail.com>
2026-04-01 15:13:33 +0000

    @ 2026-04-01T15:13:33Z

---
+++ net.acmicpc/1496/a.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+#include<sstream>
+#include<string>
+#include<string_view>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<memory>
+#include<array>
+#include<tuple>
+#include<bitset>
+#include<map>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<functional>
+#include<algorithm>
+#include<stdfloat>
+#include<cmath>
+#include<cstring>
+#define AFESDJPOI asm("")
+//$(pwd); cat <<AFESDJPOI
+
+using namespace std;
+
+using u1=uint8_t;	using u2=uint16_t;	using u4=uint32_t;	using u8=uint64_t;	using u16=unsigned __int128;
+using i1=int8_t;	using i2=int16_t;	using i4=int32_t;	using i8=int64_t;	using i16=__int128;
+										using f4=float32_t;	using f8=float64_t;	using f16=float128_t;
+using uf1=uint_fast8_t;	using uf2=uint_fast16_t;using uf4=uint_fast32_t;using uf8=uint_fast64_t;
+using if1=int_fast8_t;	using if2=int_fast16_t;	using if4=int_fast32_t;	using if8=int_fast64_t;
+
+#ifdef ONLINE_JUDGE
+constexpr bool debug=false;
+#else
+constexpr bool debug=true;
+#endif
+
+#ifdef ONLINE_JUDGE
+#define DEBUG_MACRO(x)
+#define DEBUG_MACRO_ELSE(x) x
+#else
+#define DEBUG_MACRO(x) x
+#define DEBUG_MACRO_ELSE(x)
+#endif
+
+#define DEBUG if constexpr(debug)
+
+int main(){
+	cin.tie(0)->sync_with_stdio(false);
+
+	u4 n;
+	array<array<u4, 64>, 50> a;
+	cin>>n;
+	for(u4 i=n; i--;) cin>>a[i][1];
+
+	function<u4(const u4, const u4)> f, g;
+	f = [&](const u4 p, const u4 d) -> u4 {
+		if(d==1) return a[p][1];
+		if(a[p][d]) return a[p][d];
+
+		u4 r=0;
+		for(u4 i=d; i--;){
+			r = max(
+				r,
+				g((p+(i+1)%d)%n, d-1)
+				+ a[(p+i)%n][1]
+			);
+		}
+
+		return a[p][d] = r;
+	};
+
+	g = [&](const u4 p, const u4 d) -> u4 {
+		if(d==1) return 0;
+		if(a[p][d]) return a[p][d];
+
+		u4 r=0, mx=0;
+		for(u4 i=d; i--;){
+			if(u4 v = a[(p+i)%n][1]; mx < v){
+				mx = v;
+				r = f((p+(i+1)%d)%n, d-1);
+			}
+		}
+
+		return a[p][d] = r;
+	};
+
+	u4 r = 0;
+	for(u4 i=n; i--;)
+		r = max(r, f(i, n));
+
+	cout<<r;
+
+	return 0;
+}
+AFESDJPOI
+;
